fbde29c55782439b82857901b19a9ccbf608aadc
Ashly LaMarr <surlymae@gmail.com>
2022-07-11 16:02:22 -0600

    Add first endpoint

--- README.md
+++ README.md
@@ -45,6 +45,16 @@ $ npm run start:dev
 $ npm run start:prod
 ```
 
+## Starting the database container
+
+````bash
+# in its own terminal
+$ docker-compose up dev-db
+
+# in the background
+$ docker-compose up dev-db -d
+
+
 ## Test
 
 ```bash
@@ -56,7 +66,7 @@ $ npm run test:e2e
 
 # test coverage
 $ npm run test:cov
-```
+````
 
 ## Support
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
-  login() {}
+  constructor(private readonly prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
 
-  signup() {}
+    const user = await this.prisma.user.create({
+      data: { email: dto.email, hash },
+      select: { id: true, email: true, createdAt: true },
+    });
+
+    return user;
+  }
+
+  login() {
+    return 'service login';
+  }
 }
